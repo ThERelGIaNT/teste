7ab7dada46ae332393fe8b650d41e6a63783ca31
Wesley Bule <bulewesley7@gmail.com>
2025-09-07 12:53:49 +0000

    function

---
+++ teste.py
@@ -0,0 +1,11 @@
+def saudar(mensagem):
+    print(mensagem)
+
+def dados(nome,idade,email):
+    print(f'''
+    Nome: {nome}
+    Idade: {idade}
+    Email: {email}''')
+
+saudar("ola")
+dados("Wesley" , 17 , "bulewesleu@gmail.com")
